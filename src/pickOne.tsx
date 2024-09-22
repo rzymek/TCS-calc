d6cca0b626d2f850c0f342a5612d573406234314
Krzysztof Rzymkowski <krzysztof.rzymkowski@snowflake.com>
2024-09-22 23:04:02 +0200

    cleanup

--- src/pickOne.tsx
+++ src/pickOne.tsx
@@ -1,7 +1,7 @@
 import {Pick, PickProps} from "./pick.tsx";
 import {useContext} from "preact/compat";
 import {Context} from "./context.tsx";
-import {firepowerDef, pickOneDefs, SingleSelectionFields} from "./state.ts";
+import {pickOneDefs, SingleSelectionFields} from "./state.ts";
 
 export function PickOne(props: PickProps & { field: SingleSelectionFields }) {
     const context = useContext(Context);
@@ -14,26 +14,5 @@ export function PickOne(props: PickProps & { field: SingleSelectionFields }) {
                  selected={[selected]}
                  onClick={(v: string | number) => {
                      context.update({[props.field]: selected === v ? undefined : v});
-                 }}/>;
-}
-
-export function PickFirepower(props: PickProps & { index: number }) {
-    const context = useContext(Context);
-    const {label, values} = firepowerDef(props.index);
-    const selected = context.state.firepower[props.index];
-    return <Pick {...props}
-                 label={label}
-                 values={values}
-                 selected={[selected]}
-                 onClick={(v: '+' | number) => {
-                     const firepower = [...context.state.firepower];
-                     if (v === '+') {
-                         firepower.push(undefined);
-                     } else if (v > 0) {
-                         firepower[props.index] = v;
-                     } else if (v === 0) {
-                         firepower.splice(props.index, 1);
-                     }
-                     context.update({firepower});
                  }}/>;
 }
